7e6e76d6514263029c34d4cdde609ca8356c953f
Sujan Maharjan <sujan.maharjan@pointclickcare.com>
2018-04-19 14:08:23 -0500

    Commit - 1

---
+++ src/main/java/com/cellit/client/CellitClientApplication.java
@@ -0,0 +1,12 @@
+package com.cellit.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CellitClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CellitClientApplication.class, args);
+	}
+}
---
+++ src/main/java/com/cellit/client/config/WebConfig.java
@@ -0,0 +1,50 @@
+package com.cellit.client.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.servlet.config.annotation.*;
+
+/**
+ * @author Sujan Maharjan
+ * @since 4/19/2018
+ */
+@EnableWebMvc
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        super.addViewControllers(registry);
+
+        registry.addViewController("/")
+            .setViewName("forward:/index");
+
+        registry.addViewController("/index");
+        registry.addViewController("/secure");
+
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+            .addResourceLocations("/resources/");
+    }
+
+    @Bean
+    public RequestContextListener requestContextListener() {
+        return new RequestContextListener();
+    }
+
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
